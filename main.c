fbbb6b066f5ca9ca0563f8aaf8877de3d409ab13
Gordon Young <gyoung@debian.corp.acme.local>
2018-04-29 10:49:40 -0700

    update http client

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <openssl/err.h>
 #include <openssl/bio.h>
 
-#define APIKEY "some-key"
+//#define APIKEY "some-key"
 #define HOST "tntlvnzzqsz.SANDBOX.verygoodproxy.com"
 #define PORT "443"
 
@@ -16,42 +16,25 @@ int main() {
     SSL* ssl;
     SSL_CTX* ctx;
 
-    //
-    //   Registers the SSL/TLS ciphers and digests.
-    //
-    //   Basically start the security layer.
-    //
+    //   Initialize OpenSSL
     SSL_library_init();
 
-    //
-    //  Creates a new SSL_CTX object as a framework to establish TLS/SSL
-    //  or DTLS enabled connections
-    //
+    //  Create a new SSL_CTX object as a framework to establish TLS/SSL transport
     ctx = SSL_CTX_new(TLS_client_method());
 
-    //
-    //  -> Error check
-    //
+    //  -> Error checks
     if (ctx == NULL)
     {
         printf("Ctx is null\n");
     }
 
-    //
     //   Creates a new BIO chain consisting of an SSL BIO
-    //
     bio = BIO_new_ssl_connect(ctx);
 
-    //
-    //  Use the variable from the beginning of the file to create a
-    //  string that contains the URL to the site that you want to connect
-    //  to while also specifying the port.
-    //
+    //  set the VGS Proxy URL to perform a rewrite of our POST method:
     BIO_set_conn_hostname(bio, HOST ":" PORT);
 
-    //
     //   Attempts to connect the supplied BIO
-    //
     if(BIO_do_connect(bio) <= 0)
     {
         printf("Failed connection\n");
@@ -65,70 +48,52 @@ int main() {
     //
     //  The bare minimum to make a HTTP request.
     //
-    char* write_buf = "POST / HTTP/1.1\r\n"
+    char* write_buf = "POST /post HTTP/1.1\r\n"
                       "Host: " HOST "\r\n"
-                      "Authorization: Basic " APIKEY "\r\n"
+                      //"Authorization: Basic " APIKEY "\r\n"
+                      "Content-type: application/json""\r\n"
+                      //"{\"CCN\": \"41111111111111111\"}"
+                      //"{\"CCN\": \"41111111111111111\"}"
                       "Connection: close\r\n"
                       "\r\n";
 
-    //
     //   Attempts to write len bytes from buf to BIO
-    //
     if(BIO_write(bio, write_buf, strlen(write_buf)) <= 0)
     {
-        //
         //  Handle failed writes here
-        //
         if(!BIO_should_retry(bio))
         {
-            // Not worth implementing, but worth knowing.
+            // should we retry?
         }
 
-        //
-        //  -> Let us know about the failed writes
-        //
+        //  -> Lets us know about the failed writes
         printf("Failed write\n");
     }
 
-    //
-    //  Variables used to read the response from the server
-    //
+    //  Collect the response from the server
     int size;
     char buf[1024];
 
-    //
-    //  Read the response message
-    //
+    //  Do something with the repsonse
     for(;;)
     {
-        //
         //  Get chunks of the response 1023 at the time.
-        //
         size = BIO_read(bio, buf, 1023);
 
-        //
-        //  If no more data, then exit the loop
-        //
+        //  If no more data, then we are done..
         if(size <= 0)
         {
             break;
         }
 
-        //
-        //  Terminate the string with a 0, to let know C when the string
-        //  ends.
-        //
+        //  Terminate the string with a 0, to let know C when the string ends.
         buf[size] = 0;
 
-        //
         //  ->  Print out the response
-        //
         printf("%s", buf);
     }
 
-    //
-    //  Clean after ourselves
-    //
+    //  Clean up!
     BIO_free_all(bio);
     SSL_CTX_free(ctx);
 
